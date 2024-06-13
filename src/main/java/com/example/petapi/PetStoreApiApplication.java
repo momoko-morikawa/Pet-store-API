93d81d4ebbee8508ee599443f50cfdd696ff292d
momoko.morikawa <morikawa.momoko@itfllc.co.jp>
2024-06-13 13:57:55 +0900

    firstcommit

---
+++ src/main/java/com/example/petapi/PetStoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.petapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetStoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetStoreApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/petapi/entity/Pet.java
@@ -0,0 +1,5 @@
+package com.example.petapi.entity;
+
+public class Pet {
+	
+}
